d89a80a529835c9b7ec42b7a56ff6d975f7523d3
Atokpassuzan <sujansapkota777@gmail.com>
2021-07-18 01:07:58 +0545

    username and password hidden

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://Sujan:379379@project-wiqmt.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://username:password@project-wiqmt.mongodb.net/test?retryWrites=true&w=majority",
 };
