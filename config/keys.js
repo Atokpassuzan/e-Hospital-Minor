dd0ad9c728fcd7b2e10b35a19dea1ef84c9ca944
Sujan Sapkota <Sujansapkota7777@gmail.com>
2022-09-07 20:44:12 +0545

    env changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-module.exports = {
-  MongoURI:
-    "mongodb+srv://username:password@project-wiqmt.mongodb.net/test?retryWrites=true&w=majority",
-};
+// module.exports = {
+//   MongoURI:
+//     "mongodb+srv://username:password@project-wiqmt.mongodb.net/test?retryWrites=true&w=majority",
+// };
